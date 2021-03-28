51710716a5f802571976dd9060d78edf44b3c207
kazuyamarino <vikry.yuansah@gmail.com>
2021-03-28 15:04:20 +0700

    minor fix

--- .cli/nsy.settingup.sh
+++ .cli/nsy.settingup.sh
@@ -12,11 +12,11 @@ run_setup() {
 			cp docs/apache/for_root/.htaccess ./.htaccess
 			cp .cli/tmp/env.example ./env
 			cp .cli/tmp/env.example docs/env.example/env.example
-			cp .cli/tmp/system.js ./public/js/config/system.js
+			cp .cli/tmp/system.js ./public/assets/js/config/system.js
 			cp .cli/tmp/default ./docs/nginx/sites-enabled/default
 			sed -i "s/shyffon/$filename/g" ./env
 			sed -i "s/shyffon/$filename/g" ./docs/env.example/env.example
-			sed -i "s/shyffon/$filename/g" ./public/js/config/system.js
+			sed -i "s/shyffon/$filename/g" ./public/assets/js/config/system.js
 			sed -i "s/shyffon/$filename/g" ./docs/nginx/sites-enabled/default
 
 			printf "Please wait...\n"
